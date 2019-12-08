69d0aa7ab6af8391389968303e3d7b92ecf42db6
AtsushiMiyazaki <miyazaki@eversystem.co.jp>
2019-12-08 22:46:41 +0900

    initial commit

--- rust-iost/src/iost.rs
+++ rust-iost/src/iost.rs
@@ -1,3 +1,8 @@
+use bs58;
+use ed25519_dalek::PublicKey;
+use ed25519_dalek::SecretKey;
+use ed25519_dalek::Signature;
+use ed25519_dalek::Keypair;
 
 pub struct Options {
     pub server: String,
@@ -12,6 +17,38 @@ pub struct Options {
 	pub check_result_delay: u32,
     pub check_result_max_retry: i32,
 }
+
+pub struct Action<T> {
+    contract: String,
+    action_name: String,
+    data: T
+}
+
+pub struct AmountLimit {
+    token: String,
+    value: f64
+}
+
+pub struct Sig {
+    algorithm: String,
+    signature: String,
+    public_key: String,
+}
+
+pub struct Transaction<T> {
+    pub time: i64,
+    pub expiration: i64,
+    pub gas_ratio: f64,
+    pub gas_limit: f64,
+    pub delay: i64,
+    pub chain_id: u32,
+    pub actions: Vec<Action<T>>,
+    pub amount_limit: Vec<AmountLimit>,
+    pub publisher: String,
+    pub publisher_sigs: Vec<Sig>
+}
+
+pub type PledgeAction = (u32, String, String);
 
 pub struct IOST {
     pub options: Options,
@@ -58,6 +95,80 @@ impl IOST {
         self.options.account_name = account_name;
         self
     }
+
+    pub fn set_chain_id(&mut self, chain_id: u32) -> &Self{
+        self.chain_id = chain_id;
+        self
+    }
+
+    pub fn set_tx_options(&mut self, gas_limit: u64, gas_ratio: u64, expiration: u64) -> &Self{
+        self.options.gas_limit = gas_limit;
+        self.options.gas_ratio = gas_ratio;
+        self.options.expiration = expiration;
+        self
+    }
     
+    pub fn set_sign_algorithm(&mut self, sign_algorithm: String)-> Result<&Self, ()>{
+        if sign_algorithm != String::from("ed25519") ||
+           sign_algorithm != String::from("SECP256K1")
+        {
+            return Err(())
+        }
+
+        self.options.sign_algorithm = sign_algorithm;
+        Ok(self)
+    }
+
+    pub fn set_use_longest_chain(&mut self, use_longest_chain : bool) -> &Self{
+        self.use_longest_chain = use_longest_chain;
+        self
+    }
+
+
+    pub fn create_tx_byte(){
+        let s = bs58::decode("2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1").into_vec().unwrap();
+
+        let seckey: SecretKey =  SecretKey::from_bytes(&s).unwrap();
+        let pubkey: PublicKey = (&seckey).into();
+        let keypair =  Keypair {
+            secret: seckey,
+            public: pubkey
+        };
+
+        let actions = vec!( Action {
+                contract: String::from("gas.iost"),
+                action_name: String::from("pledge"),
+                data: (100, String::from("admin"), String::from("admin"))
+            });
+        let amount_limit = vec!( AmountLimit {
+            token: String::from("iost"),
+            value: 100.0
+        });
+
+        let sig = vec!( Sig{
+            algorithm: String::from("ED25519"),
+            signature: String::from(""),
+            public_key: String::from("")
+        });
+
+        let tx = Transaction {
+            time: 1575804827,
+            expiration: 1585804827,
+            gas_ratio: 1.0,
+            gas_limit: 100000.0,
+            delay: 0,
+            chain_id: 1020,
+            actions,
+            amount_limit,
+            publisher: String::from("admin"),
+            publisher_sigs: sig
+        };
+    }
+
+    fn _bytes(&tx: Transaction<T>) -> Vec<u8>{
+
+    };
 }
+
+
 
--- rust-iost/src/lib.rs
+++ rust-iost/src/lib.rs
@@ -1,12 +1,16 @@
+pub mod iost;
+
+
 #[cfg(test)]
 mod tests {
     use super::iost;
+    use bs58;
     #[test]
     fn it_works() {
-        let i = iost::init();
-        assert_eq!(i.options.server, "127.0.0.1");
+        let i = iost::IOST::dec();
+        println!("{:?}", i);
+        let s = bs58::decode("2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1").into_vec().unwrap();
+        assert_eq!(i, );
     }
     
 }
-
-pub mod iost;
