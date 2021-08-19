e5d227d7f696d810f097491edf7e90e6c83e53cd
Atsushi Miyazaki <ki929.oa95cp@gmail.com>
2021-08-19 23:41:00 +0900

    done impl

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,3 +1,25 @@
+use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+
+    let rnum = rand::thread_rng().gen_range(0..101);
+
+    loop {
+        let mut buf = String::new();
+        io::stdin()
+            .read_line(&mut buf)
+            .expect("Failed to read line");
+
+        let guess: u8 = buf.trim().parse().expect("Expected number");
+        match guess.cmp(&rnum) {
+            Ordering::Less => println!("Small"),
+            Ordering::Equal => {
+                println!("Exactly!");
+                break;
+            },
+            Ordering::Greater => println!("Big"),
+        }
+    }
 }
